20ecdf9979c0b25236f7d52cfc3b5b354c9f1187
Алексей <praldmur@gmial.com>
2025-08-21 20:14:40 +0300

    Внесены изменения

--- 1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data = [
-    {'id': 1, 'name': 'Joe'},
-    {'id': 2, 'name': 'Henry'},
-    {'id': 3, 'name': 'Sam'},
-    {'id': 4, 'name': 'Max'}
-]
-customers = pd.DataFrame(data)
-customers.head()
